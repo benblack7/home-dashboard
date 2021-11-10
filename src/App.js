26eb5a26bd597ccaa1a7421e66cf14a90c1fbcb9
Ben Black <benblack7@gmail.com>
2021-11-10 15:08:58 -0600

    Added Automatic Dark Mode Switching

--- src/App.js
+++ src/App.js
@@ -1,36 +1,71 @@
 import './App.css';
+import * as React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import Weather from './components/Weather.js';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Good Morning!
-      </header>
-      <div>
-        Insert Weather
-        <Weather />
-      </div>
-      <div>
-        Today's Calendar
-      </div>
-      <div>
-        Traffic Conditions
-      </div>
-      <div>
-        Top Stories
-      </div>
-      <div>
-        Router UP/DOWN Metrics
-      </div>
-      <div>
-        Plex Stats
-      </div>
-      <div>
-        Unread Emails
-      </div>
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-    </div>
+const theme = React.useMemo(
+  () =>
+    createTheme({
+      palette: {
+        mode: prefersDarkMode ? 'dark' : 'light',
+      },
+    }),
+  [prefersDarkMode],
+);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Item>Good Morning!</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Insert Weather</Item>
+        <Grid item xs={4}>
+          <Item><Weather /></Item>
+        </Grid>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Today's Calendar</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Traffic Conditions</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Top Stories</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Router UP/DOWN Metrics</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Plex Stats</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Unread Emails</Item>
+      </Grid>
+
+    </Grid>
+    </ThemeProvider>
   );
 }
 

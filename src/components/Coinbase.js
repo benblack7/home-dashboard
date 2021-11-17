ea03b174f8a46fb5af7905cd793abf8b76d97bc6
Ben Black <benblack7@gmail.com>
2021-11-16 18:19:49 -0600

    Added GraphQL query

--- src/components/Coinbase.js
+++ src/components/Coinbase.js
@@ -9,24 +9,38 @@ import {
 import Grid from '@mui/material/Grid';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://o5x5jzoo7z.sse.codesandbox.io/',
   cache: new InMemoryCache()
 });
 
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(gql`
-    {
-      rates(currency: "USD") {
-        currency
-        rate
-      }
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
     }
-  `);
+  }
+`;
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return data.rates.map(({ currency, rate }) => (
+  return data.rates.slice(0,3).map(({ currency, rate }) => (
     <div key={currency}>
       <p>
         {currency}: {rate}
